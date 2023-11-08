7cfec397b5470bb4ea5c0bc098647afe19b9ef72
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2023-11-08 20:49:37 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1> Vics here</h1>, document.getElementById('root'));
+
+function Card(props){
+    return (
+     <div className="card">
+          <div className="card-inner">
+            <div className="card-front">
+                <img src={props.image} className="image-responsive" alt='project-name' />
+            </div>
+            <div className="card-back">
+              <a href= {props.link} target="_blank" rel="noreferrer" alt=" JavaScript- exercises" >
+              <p> {props.name}</p>
+            </a>
+            </div>
+          </div>
+        </div>
+    );
+
+};
+
+
+ReactDOM.render(
+<div> 
+<h3> My Project</h3>
+<div className='cards'>
+            <Card  
+            name ="Javescript- exercises"
+            image="images/javascript.JPG"
+            link="https://astonishing-pasca-5a36aa.netlify.app/"
+            language="#CSS #HTML #Express.js #Node.js"
+            />
+            <Card 
+            name ="Personal Project"
+            image="images/react.JPG"
+            link="https://www.victoriamoonteanu.com/"
+            language="#CSS #HTML #Express.js #Node.js"
+            />
+            <Card 
+            name ="Site Template"
+            image="images/extraterestrial .png"
+            link="https://vicmoon.github.io/CSS_SITE/"
+            language="#CSS #HTML #Responsive #Template"
+            />
+</div>
+ </div>, 
+ document.getElementById('root'));
 
  

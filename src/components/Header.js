56a4af2b1fcb26f341699f53386d7d54b88636ab
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2023-11-15 22:03:23 +0100

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ function Header(){
         <>
         <nav className="navbar">
                 <ul>
-                    <li><a className="navbar-brand" href="../pages/home">Home</a></li>
+                    <li><a className="navbar-brand" href="/home">Home</a></li>
                     <li><a className="navbar-brand" href="/about">About</a></li>
                     <li><a className="navbar-brand" href="/work">Projects</a></li>
                     <li><a className="navbar-brand" href="/contact">Contact</a></li>

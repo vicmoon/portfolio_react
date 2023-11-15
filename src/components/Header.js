1ad6b295e0d6c0525b6601a80c62f6e49dd71bb0
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2023-11-15 21:47:00 +0100

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Work from './pages/Work';
+import NoPage from '../src/pages/NoPage';
+
 
 
 

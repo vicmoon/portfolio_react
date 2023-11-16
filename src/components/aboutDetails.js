4932886b7b4cad7d6e582c0599ce8769a9daa351
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2023-11-16 08:42:22 +0100

    update workDetials

--- src/components/aboutDetails.js
+++ src/components/aboutDetails.js
@@ -2,7 +2,7 @@ const aboutDetails = [
     {
         id: 1,
         title: "About Me",
-        description: "I lived and worked in over 10 countries supporting myself through professional modelling. After a few years, I decided to concentrate on something else. My intention while experimenting with different jobs, cultures and skills is to be myself and improve my world. I speak English, German, Romanian and Italian.",
+        description: "I have lived and worked in over 10 countries supporting myself through professional fashion modelling. After a few years of traveling, I decided to concentrate on something else. To this day, my intention while experimenting with different jobs, cultures and skills is to be myself and improve my world. I speak English, German, Romanian and Italian..",
 
     },
 
@@ -10,14 +10,14 @@ const aboutDetails = [
     {
         id: 2,
         title: "Education",
-        description: "I studied at Ovidius University in Constanta Romania, where I graduated with a Bachelor of Arts in English and German. I also studied in Italy at Scuola Superiore per Mediatori Linguistici, Pisa, where I graduated with a qualification in English - Italian conference interpreting. I have also graduated from SheCodes, an online school teaching women to code. My formal education has given me a strong foundation in the English and German languages, as well as in the Italian language, at the same time, my practical experiences have given me the opportunity to develop my communication skills and my ability to work in a team."
+        description: "I studied at Ovidius University in Constanta, Romania where I graduated with a Bachelor of Arts in English and German. I also studied in Italy at Scuola Superiore per Mediatori Linguistici, Pisa, where I graduated with a qualification in English - Italian conference interpreting. I have also graduated from SheCodes, an online school teaching women to code. My formal education has given me a strong foundation in the English, Italian and German languages. At the same time, my practical experiences have given me the opportunity to develop my communication skills and my ability to work in a team."
 
     },
 
     {
         id: 3,
         title: "Skills",
-        description: "I am a creative person with a passion for learning. I am a fast learner and I am not afraid to try new things. I am a good communicator and I enjoy working in a team. Also, I am a listener and I am not afraid to ask questions."
+        description: "I am a creative person with a passion for learning and reading. I consider myself to be a fast learner and who's not afraid to try new things. I am a good communicator and I enjoy working in a team. Also, I am a listener and I am not afraid to ask questions about what I'm hearing."
 
     },
 

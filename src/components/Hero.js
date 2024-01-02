a80d5db53f999aca56bfc390a9edb8f3fcd5fc68
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-01-02 21:18:06 +0100

    update hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ function Hero(){
           <h1>Hi, I'm Victoria</h1>
           <div>
                 <img
-                    src="../images/victoria_new.jpg
+                    src="../images/victoria_m.jpg
                     "
                     className="img-fluid"
                     width="300px"

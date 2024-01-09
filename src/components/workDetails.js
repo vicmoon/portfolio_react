874eec0cad0bfcd014f0e54f47c2e44271fd1767
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-01-09 20:28:09 +0100

    update projects

--- src/components/workDetails.js
+++ src/components/workDetails.js
@@ -7,7 +7,7 @@ const details = [
         link:"https://ornate-halva-c5686c.netlify.app/",
         language:"#DOM #Class: manipulation, #JavaScript"
     },
-    {   id : 1,
+    {   id : 2,
         name :"Guess the number!",
         image:"images/Guess_the_number.png",
         link:"https://soft-pika-ed6b48.netlify.app/",
@@ -15,14 +15,14 @@ const details = [
     },
     
     
-    {   id : 2,
+    {   id : 3,
         name :"Javascript- exercises",
         image:"images/javascript.JPG",
         link:"https://astonishing-pasca-5a36aa.netlify.app/",
         language:"#CSS #HTML #Express.js #Node.js"
     },
 
-    {   id : 3,
+    {   id : 4,
         name: "Creative Project",
         image:"images/journey1.jpg",
         link: "https://www.victoriamoonteanu.com/",
@@ -30,7 +30,7 @@ const details = [
 
     },
     {
-        id : 4,
+        id : 5,
         name:"Site Template",
         image:"images/extraterestrial .png",
         link:"https://vicmoon.github.io/CSS_SITE/",
@@ -45,13 +45,13 @@ const details = [
         language:"#CSS #HTML #JavaScript #Game"
 
 },
-{       id : 6,
+{       id : 7,
         name:"React Dictionary",
         image:"images/Dictionary.JPG",
         link:"https://inspiring-tanuki-a897c7.netlify.app/",
         language:"#CSS #API #HTML #JavaScript #React"
 },
-{       id : 7,
+{       id : 8,
         name:"Creative Project",
         image:"images/creatives.JPG",
         link:"https://playful-cuchufli-58a84f.netlify.app/",
@@ -59,21 +59,21 @@ const details = [
 
 },
 
-{       id : 8,
+{       id : 9,
         name:"World Clock App",
         image:"images/clock.JPG",
         link:"https://phenomenal-unicorn-f160f9.netlify.app/",
         language:"#CSS #HTML #JavaScript #API #Responsive #moment.js>"
 },
 
-{       id : 9,   
+{       id : 10,   
         name :"First Landing Page",
         image:"images/newlanding page.JPG",
         link:"https://s3.amazonaws.com/shecodesio-production/challenge_submissions/files/001/286/709/original/basicaddon.html?1668786913",
         language:"#CSS #HTML #Responsive"
 
 },
-{       id : 10,
+{       id : 11,
         name:"Weather App in React",
         image:"images/weather.JPG ",
         link:"https://illustrious-beignet-228d77.netlify.app/ ",
@@ -81,13 +81,13 @@ const details = [
 
 },
 
-{       id : 11,
+{       id : 12,
         name :"JavaScript Weather App",
         image:"images/vanilajs.JPG ",
         link:"https://vanillaweathersearch.netlify.app/ ",
         language:" #JavaScript."
 },
-{       id : 12,
+{       id : 13,
         name :"Simple Landing Page",
         image:"images/reading.JPG",
         link:"https://s3.amazonaws.com/shecodesio-production/challenge_submissions/files/000/874/197/original/Finalproject.html?1653155333/ ",

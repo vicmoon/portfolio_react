94b1123c3ca81bbdce14d72edc2840d0284bf823
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-05-08 20:05:07 +0200

    update certifications

--- src/pages/Certifications.js
+++ src/pages/Certifications.js
@@ -8,9 +8,9 @@ const Certifications = () => {
         <Header/>
           <>
           <div id="certifications">
-            <a href= "https://www.codecademy.com/profiles/victoria_moon/certificates/5ddbff3dd1bd3b001397ae9f" rel="noreferrer" target="_blank">[Create Video Games with Phaser.js]</a>
+            <a href= "https://www.codecademy.com/profiles/victoria_moon/certificates/5ddbff3dd1bd3b001397ae9f" rel="noreferrer" target="_blank">[Video Games with Phaser.js]</a>
             <a href= "https://www.codecademy.com/profiles/victoria_moon/certificates/705dcb15de0da4dd9d9fc4f3274b430e " rel="noreferrer" target="_blank">[JavaScript Course]</a>
-            <a href= "https://www.udemy.com/certificate/UC-e2ceb578-0101-4c81-95d8-fdf81aa4356b/?utm_campaign=email&utm_medium=email&utm_source=sendgrid.com" rel="noreferrer"target="_blank">[The Complete JavaScript Course 2024]</a>
+            <a href= "https://www.udemy.com/certificate/UC-e2ceb578-0101-4c81-95d8-fdf81aa4356b/?utm_campaign=email&utm_medium=email&utm_source=sendgrid.com" rel="noreferrer"target="_blank">[Complete JavaScript 2024]</a>
             <a href= "https://www.udemy.com/certificate/UC-9b317dcd-221c-44fb-b243-bf0ce5aaeba9/" rel="noreferrer"target="_blank">[Web Development Bootcamp]</a> 
             <a href= "https://www.udemy.com/certificate/UC-998d744f-bb26-4e58-89a1-d544e9fe33d4/" rel="noreferrer"target="_blank">[ServiceNow Developer]</a>
             <a href= "https://www.shecodes.io/certificates/39116-victoria-munteanu/max" rel="noreferrer"target="_blank">[Front-end Developer]</a> 

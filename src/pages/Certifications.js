b0b2a3204250eba22d896776e7a1f96cedb437d3
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-05-09 19:26:19 +0200

    update

--- src/pages/Certifications.js
+++ src/pages/Certifications.js
@@ -15,8 +15,6 @@ const Certifications = () => {
             <a href= "https://www.udemy.com/certificate/UC-998d744f-bb26-4e58-89a1-d544e9fe33d4/" rel="noreferrer"target="_blank">[ServiceNow Developer]</a>
             <a href= "https://www.shecodes.io/certificates/39116-victoria-munteanu/max" rel="noreferrer"target="_blank">[Front-end Developer]</a> 
             <p >[Zertifikat Deutsch C1]</p> 
-
-            
          </div>
           </>
         <Footer />

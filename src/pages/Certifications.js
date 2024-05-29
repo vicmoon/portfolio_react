f06fef0428447368941c884c413720acd01856e2
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-05-29 21:01:15 +0200

    update

--- src/pages/Certifications.js
+++ src/pages/Certifications.js
@@ -1,34 +1,94 @@
 import Footer from "../components/Footer";
-import Header  from "../components/Header";
+import Header from "../components/Header";
 
 const Certifications = () => {
-
-    return (
+  return (
+    <>
+      <Header />
       <>
-        <Header/>
-          <>
-          <div id="certifications">
-            <a href= "https://www.codecademy.com/profiles/victoria_moon/certificates/28ccb695c353465a8785a036ad32d3b9" rel="noreferrer" target="_blank">[Intro to Generative AI ]</a>
-            <a href= "https://www.codecademy.com/profiles/victoria_moon/certificates/ef9a06323b7d44b091a3ec7816092dc6 " rel="noreferrer" target="_blank">[How to Use ChatGPT ]</a> 
-            <a href= "https://www.codecademy.com/profiles/victoria_moon/certificates/402dfacd31314cd693b1a893fde9ca7b" rel="noreferrer" target="_blank">[How to use AI for coding ]</a> 
-            <a href= "https://www.codecademy.com/profiles/victoria_moon/certificates/512386fdc7f6c934f98b01e6afa8285a" rel="noreferrer" target="_blank">[Intermediate JavaScript]</a>
-            <a href= "https://drive.google.com/file/d/1tS6l1z0xnAfBhhtdp4sp-6Ky03G9XORM/view" rel="noreferrer" target="_blank">[ServiceNow Certified System Administrator]</a>
-            <a href= "https://www.codecademy.com/profiles/victoria_moon/certificates/5ddbff3dd1bd3b001397ae9f" rel="noreferrer" target="_blank">[Video Games with Phaser.js]</a>
-            <a href= "https://www.codecademy.com/profiles/victoria_moon/certificates/705dcb15de0da4dd9d9fc4f3274b430e " rel="noreferrer" target="_blank">[JavaScript Course]</a>
-            <a href= "https://www.udemy.com/certificate/UC-e2ceb578-0101-4c81-95d8-fdf81aa4356b/?utm_campaign=email&utm_medium=email&utm_source=sendgrid.com" rel="noreferrer"target="_blank">[Complete JavaScript 2024]</a>
-            <a href= "https://www.udemy.com/certificate/UC-9b317dcd-221c-44fb-b243-bf0ce5aaeba9/" rel="noreferrer"target="_blank">[Web Development Bootcamp]</a> 
-            <a href= "https://www.udemy.com/certificate/UC-998d744f-bb26-4e58-89a1-d544e9fe33d4/" rel="noreferrer"target="_blank">[ServiceNow Developer]</a>
-            <a href= "https://www.shecodes.io/certificates/39116-victoria-munteanu/max" rel="noreferrer"target="_blank">[Front-end Developer]</a> 
-         </div>
-          </>
-        <Footer />
-
+        <div id="certifications">
+          <a
+            href="https://www.codecademy.com/profiles/victoria_moon/certificates/28ccb695c353465a8785a036ad32d3b9"
+            rel="noreferrer"
+            target="_blank"
+          >
+            [Intro to Generative AI ]
+          </a>
+          <a
+            href="https://www.codecademy.com/profiles/victoria_moon/certificates/ef9a06323b7d44b091a3ec7816092dc6 "
+            rel="noreferrer"
+            target="_blank"
+          >
+            [How to Use ChatGPT ]
+          </a>
+          <a
+            href="https://www.codecademy.com/profiles/victoria_moon/certificates/402dfacd31314cd693b1a893fde9ca7b"
+            rel="noreferrer"
+            target="_blank"
+          >
+            [How to use AI for coding ]
+          </a>
+          <a
+            href="https://www.codecademy.com/profiles/victoria_moon/certificates/512386fdc7f6c934f98b01e6afa8285a"
+            rel="noreferrer"
+            target="_blank"
+          >
+            [Intermediate JavaScript]
+          </a>
+          <a
+            href="https://drive.google.com/file/d/1tS6l1z0xnAfBhhtdp4sp-6Ky03G9XORM/view"
+            rel="noreferrer"
+            target="_blank"
+          >
+            [ServiceNow CSA]
+          </a>
+          <a
+            href="https://www.codecademy.com/profiles/victoria_moon/certificates/5ddbff3dd1bd3b001397ae9f"
+            rel="noreferrer"
+            target="_blank"
+          >
+            [Video Games with Phaser.js]
+          </a>
+          <a
+            href="https://www.codecademy.com/profiles/victoria_moon/certificates/705dcb15de0da4dd9d9fc4f3274b430e "
+            rel="noreferrer"
+            target="_blank"
+          >
+            [JavaScript Course]
+          </a>
+          <a
+            href="https://www.udemy.com/certificate/UC-e2ceb578-0101-4c81-95d8-fdf81aa4356b/?utm_campaign=email&utm_medium=email&utm_source=sendgrid.com"
+            rel="noreferrer"
+            target="_blank"
+          >
+            [Complete JavaScript 2024]
+          </a>
+          <a
+            href="https://www.udemy.com/certificate/UC-9b317dcd-221c-44fb-b243-bf0ce5aaeba9/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            [Web Development Bootcamp]
+          </a>
+          <a
+            href="https://www.udemy.com/certificate/UC-998d744f-bb26-4e58-89a1-d544e9fe33d4/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            [ServiceNow Developer]
+          </a>
+          <a
+            href="https://www.shecodes.io/certificates/39116-victoria-munteanu/max"
+            rel="noreferrer"
+            target="_blank"
+          >
+            [Front-end Developer]
+          </a>
+        </div>
       </>
-  
-    );
-
-
-}
-
+      <Footer />
+    </>
+  );
+};
 
 export default Certifications;

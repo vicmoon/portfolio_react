b62d757ab9456d1122034024d142314184112a3c
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-06-29 18:19:01 +0200

    update alt

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,28 @@
-import Avatar from './Avatar';
+import Avatar from "./Avatar";
 
-function Card(props){
-    return (
-      <div className='front-page'>
-     <div className="card">
-          <div className="card-inner">
-            <div className="card-front">
-             <Avatar image={props.image} />
-            </div>
-            <div className="card-back">
-              <a href= {props.link} target="_blank" rel="noreferrer" alt=" JavaScript- exercises" >
+function Card(props) {
+  return (
+    <div className="front-page">
+      <div className="card">
+        <div className="card-inner">
+          <div className="card-front">
+            <Avatar image={props.image} />
+          </div>
+          <div className="card-back">
+            <a
+              href={props.link}
+              target="_blank"
+              rel="noreferrer"
+              alt="Project card"
+            >
               <p>{props.name}</p>
               <p>{props.language}</p>
             </a>
-            </div>
           </div>
-      </div>
         </div>
-    );
-
-};
+      </div>
+    </div>
+  );
+}
 
 export default Card;

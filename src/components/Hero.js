79ae0064c2d01b854af93ed832a867740b7847a6
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-06-29 18:17:28 +0200

    update icon

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,26 +1,25 @@
 import Footer from "./Footer";
 
-
-function Hero(){
-    return (
-         <div className='hero'>
-          <h1> Hi, I'm Victoria</h1>
-          <div>
-                <img
-                    src="../images/victoria.jpg
+function Hero() {
+  return (
+    <div className="hero">
+      <h1> Hi, I'm Victoria</h1>
+      <div>
+        <img
+          src="../images/victoria.jpg
                     "
-                    // className="img-fluid"
-                    // width="500px"
-                    // height='500px'
-                    alt="Myself"
-                  />
-        </div>
-        <div className="hero__description">
-          <p>Self-taught junior full-stack developer</p>
-        </div>
-        <Footer />
+          // className="img-fluid"
+          // width="500px"
+          // height='500px'
+          alt="Victoria"
+        />
       </div>
-    )
+      <div className="hero__description">
+        <p>Self-taught junior full-stack developer</p>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Hero;

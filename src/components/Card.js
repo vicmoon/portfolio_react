a16c77539601a12d8ee7d37b5a9fee32e280bcc3
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-07-05 16:11:54 +0200

    update style

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,23 +2,21 @@ import Avatar from "./Avatar";
 
 function Card(props) {
   return (
-    <div className="front-page">
-      <div className="card">
-        <div className="card-inner">
-          <div className="card-front">
-            <Avatar image={props.image} />
-          </div>
-          <div className="card-back">
-            <a
-              href={props.link}
-              target="_blank"
-              rel="noreferrer"
-              alt="Project card"
-            >
-              <p>{props.name}</p>
-              <p>{props.language}</p>
-            </a>
-          </div>
+    <div className="card">
+      <div className="card-inner">
+        <div className="card-front">
+          <Avatar image={props.image} />
+        </div>
+        <div className="card-back">
+          <a
+            href={props.link}
+            target="_blank"
+            rel="noreferrer"
+            alt="Project card"
+          >
+            <p>{props.name}</p>
+            <p>{props.language}</p>
+          </a>
         </div>
       </div>
     </div>
